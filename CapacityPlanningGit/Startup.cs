8e193a09ba84e3794bb3b311a9457fbe8cd2b5ca
c0bigyv <ybiggs@yahoo.com>
2015-09-16 11:42:56 -0400

    New Project Initial Add

---
+++ CapacityPlanningGit/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CapacityPlanningGit.Startup))]
+namespace CapacityPlanningGit
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
